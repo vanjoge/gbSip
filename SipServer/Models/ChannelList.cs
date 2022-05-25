b745163364be446cd0c32b949bfb5a666a783683
佳驹 万 <wjj58200@163.com>
2022-05-25 15:49:51 +0800

    改为按ID排序

--- SipServer/Models/ChannelList.cs
+++ SipServer/Models/ChannelList.cs
@@ -9,26 +9,17 @@ namespace SipServer.Models
 {
     public class ChannelList
     {
-        private ConcurrentDictionary<string, Catalog.Item> data = new ConcurrentDictionary<string, Catalog.Item>();
-        List<Catalog.Item> lst = new List<Catalog.Item>();
-        public int Count => lst.Count;
+        SortedDictionary<string, Catalog.Item> data = new SortedDictionary<string, Catalog.Item>();
+        public int Count => data.Count;
 
         public void AddOrUpdate(Catalog.Item item)
         {
-            if (data.TryGetValue(item.DeviceID, out var old))
-            {
-                old.CopyVal(item);
-            }
-            else
-            {
-                lst.Add(item);
-                data[item.DeviceID] = item;
-            }
+            data[item.DeviceID] = item;
         }
 
         public List<Catalog.Item> ToList()
         {
-            return lst;
+            return data.Values.ToList();
         }
     }
 }
